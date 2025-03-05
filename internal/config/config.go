438957d1dfd684d7bfdb219039072a4c0ee6a7cc
MechamJonathan <jonsmec95@gmail.com>
2025-03-05 16:48:30 -0700

    read config file function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,46 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
 type Config struct {
-	DBURL string `json:db_url`
-	CurrentUserName `json:current_user_name`
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	fullpath, err := GetConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(fullpath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func GetConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	fullpath := filepath.Join(home, configFileName)
+	return fullpath, nil
+
 }
