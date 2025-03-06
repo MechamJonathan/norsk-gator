47dd7f8f75fc023f910d5f4cf2ec42a5bf4457b0
MechamJonathan <jonsmec95@gmail.com>
2025-03-06 16:13:30 -0700

    read config file

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/MechamJonathan/norsk-gator/internal/config"
+)
+
 func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %v\n", cfg)
 
 }
