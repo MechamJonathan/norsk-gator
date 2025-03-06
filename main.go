7cd738f851aa0fdfc2103b28ce903512041f3d2c
MechamJonathan <jonsmec95@gmail.com>
2025-03-06 16:38:03 -0700

    updated main

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/MechamJonathan/norsk-gator/internal/config"
 )
@@ -18,11 +19,25 @@ func main() {
 	}
 	fmt.Printf("Read config: %v\n", cfg)
 
-	err = cfg.SetUser("jon")
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
+	programState := &state{
+		cfg: &cfg,
 	}
-	fmt.Printf("Read config again: %v\n", cfg)
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
